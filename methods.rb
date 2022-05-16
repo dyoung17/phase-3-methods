e3a1d245a8137350d3e9450af906449a1e6b16c3
Daven Young <davenyoung@msn.com>
2022-05-16 11:56:56 -0600

    commit tree f891d864893d8ca39f9cb03099bff2d681627d5a

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num1)
+    begin 
+        return num1 / 2
+    rescue
+        return nil
+    end
+end
